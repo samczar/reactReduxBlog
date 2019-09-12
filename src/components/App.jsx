064a0ea6269e0ea6fa8c4ac5b7de4903b6ef3b99
AnyankaChibuzo <senetor.samczar@gmail.com>
2019-09-12 22:23:11 +0200

    Blog and User

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import PostList from './PostList'
+
+const App = () => {
+	return (
+		<div>
+			<h3>App</h3>
+			<PostList />
+		</div>
+	)
+}
+export default App
---
+++ src/components/PostList.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchPosts } from '../actions'
+import UserHeader from './UserHeader'
+
+class PostList extends Component {
+	componentDidMount() {
+		this.props.fetchPosts()
+	}
+
+	renderList = () => {
+		return this.props.posts.map(({ id, title, body, userId }, key) => {
+			return (
+				<li key={key}>
+					<h3>{title} </h3>
+					<br />
+					{body}
+
+					<h4>
+						<b>
+							<UserHeader userId={userId} />
+						</b>
+					</h4>
+				</li>
+			)
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<h5>Post List</h5>
+				<ul>{this.renderList()}</ul>
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return { posts: state.posts }
+}
+
+export default connect(
+	mapStateToProps,
+	{ fetchPosts }
+)(PostList)
